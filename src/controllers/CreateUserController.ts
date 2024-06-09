edc23b1540aa341dd8cf3b62908434730e618f1d
Guilherme <guilherme1984.araujo@gmail.com>
2024-06-09 07:42:02 -0300

    Criado o http repesponse na entity e na utils

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -5,14 +5,17 @@ class CreateUserController {
     async handle(request: Request, response: Response): Promise<Response> {
         const createUserService = new CreateUserService();
         
-        const { nome, shiftDate } = request.body;
+        const { name, shiftDate, role } = request.body;
 
-        if (!nome || !shiftDate) {
+        if (!name || !shiftDate) {
             return response.status(400).json({ message: "Preencha todos os campos" });
         }
 
         try {
-            const user = await createUserService.execute({ nome, shiftDate });
+
+            const shiftDateObj = new Date(shiftDate);
+
+            const user = await createUserService.execute({ name, shiftDate: shiftDateObj, role});
             return response.status(201).json(user);
         } catch (err) {
             return response.status(400).json({ error: err.message });
@@ -21,6 +24,13 @@ class CreateUserController {
 }
 
 export { CreateUserController };
+
+//Listar usuários
+
+export const getUsers = async (request: Request, response: Response) =>{
+    const HttpResponse = await getUsersService()
+    response.status(HttpResponse.status).json(HttpResponse.body)
+}
 
 //Atualizar a data do Plantão
 
