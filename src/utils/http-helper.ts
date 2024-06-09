7c35a2846630f8c961161082e9ef48c1b9d0631d
Guilherme <guilherme1984.araujo@gmail.com>
2024-06-09 07:48:22 -0300

    criado o status code ok

--- src/utils/http-helper.ts
+++ src/utils/http-helper.ts
@@ -1,1 +1,8 @@
-import { HttpResponse } from 
+import { HttpResponse } from "../entity/http-response-entity"
+
+export const ok = async (data:any): Promise< HttpResponse > => {
+    return {
+        statusCode: 200,
+        body: data
+    }
+}
