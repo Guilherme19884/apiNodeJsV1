75fc557fa001007eafe25163c1f421fcd26cd472
Guilherme <guilherme1984.araujo@gmail.com>
2024-06-19 17:50:39 -0300

    Criado entity shift

--- src/entities/Shift.ts
+++ src/entities/Shift.ts
@@ -1,14 +1,15 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm"
 
 @Entity()
+@Unique(["month", "shiftDate"])
 export class Shift {
 
     @PrimaryGeneratedColumn()
     id: number
 
-    @Column({ unique: true, nullable: false })
+    @Column({ nullable: false })
     mouth:number
 
-    @Column({ unique: true, nullable: false  })
+    @Column({ nullable: false  })
     shiftDate: Date
 }
